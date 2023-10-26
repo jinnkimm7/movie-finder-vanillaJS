6408992b5097a7ab8aa32ca9236eecf827885575
Jin <jinnkimm7@gmail.com>
2023-10-26 15:52:24 +0900

    Feat: movieStore

---
+++ src/store/movie.js
@@ -0,0 +1,23 @@
+import Store from '../core/Store';
+
+const store = new Store({
+  searchText: '',
+  page: 1,
+  movies: [],
+});
+
+export default store;
+
+export const searchMovies = async page => {
+  if (page === 1) {
+    store.state.page = 1;
+    store.state.movies = [];
+  }
+  const API_KEY = '7035c60c';
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${store.state.searchText}&page=${store.state.page}`);
+  const { Search } = await res.json();
+  store.state.movies = [
+    ...store.state.movies,
+    ...Search
+  ];
+}
